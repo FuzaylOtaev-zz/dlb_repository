f7c996703a2019d0fdba8f8220477f8103466a39
fuzayl <otaev.fuzayl@gmail.com>
2020-04-11 21:36:44 +0500

    Main class added

---
+++ src/uz.tuit/Main.java
@@ -0,0 +1,11 @@
+package uz.tuit;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(max(10, 20));
+    }
+
+    public static int max(int a, int b) {
+        return Math.max(a, b);
+    }
+}
